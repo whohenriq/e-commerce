0609b3d07f705884ce5acc50c060b418e34581ec
Henrique Eduardo <heduardocr7@gmail.com>
2025-01-26 04:26:31 -0300

    refactor: fix import

--- src/main/java/br/edu/ifrn/e_commerce/config/SwaggerConfig/OpenApiConfig.java
+++ src/main/java/br/edu/ifrn/e_commerce/config/SwaggerConfig/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package br.edu.ifrn.e_commerce.config.swagger;
+package br.edu.ifrn.e_commerce.config.SwaggerConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
